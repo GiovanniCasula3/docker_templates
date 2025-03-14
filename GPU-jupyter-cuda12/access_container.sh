25d859baa8e713a84e1803c3fdbc741546c9d0ba
Dani <d.galgora@upm.es>
2025-03-14 16:23:05 +0100

    [CUDA][FIX] cuda 12 access container script

--- GPU-jupyter-cuda12/access_container.sh
+++ GPU-jupyter-cuda12/access_container.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
 
-#JUPYTERLAB_CONTAINER_NAME=$(grep '^JUPYTERLAB_CONTAINER_NAME=' .env | cut -d '=' -f2 | tr -d ' "')
+JUPYTERLAB_CONTAINER_NAME=$(grep '^JUPYTERLAB_CONTAINER_NAME=' .env | cut -d '=' -f2 | tr -d ' "')
 
-#if [ -z "$JUPYTERLAB_CONTAINER_NAME" ]; then
-#  echo "[!] Error: Variable JUPYTERLAB_CONTAINER_NAME not defined in .env."
-#  exit 1
-#fi
+if [ -z "$JUPYTERLAB_CONTAINER_NAME" ]; then
+  echo "[!] Error: Variable JUPYTERLAB_CONTAINER_NAME not defined in .env."
+  exit 1
+fi
 
-# docker exec -it -u 0 "$JUPYTERLAB_CONTAINER_NAME" bash
-
-docker exec -it -u $UID jupyterlab_gpu_dalgora bash
+docker exec -it -u 0 "$JUPYTERLAB_CONTAINER_NAME" bash
